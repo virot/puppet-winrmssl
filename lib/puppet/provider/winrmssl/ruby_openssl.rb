69c369a272f2542759407e4e74e98a64713b7aa9
Oscar Virot <virot@virot.com>
2021-09-27 21:06:39 +0200

    Update ruby_openssl.rb
    Exclude certificates not containing Server Authentication

--- lib/puppet/provider/winrmssl/ruby_openssl.rb
+++ lib/puppet/provider/winrmssl/ruby_openssl.rb
@@ -31,6 +31,7 @@ Puppet::Type.type(:winrmssl).provide(:ruby_openssl) do
     issuer_subject.gsub!(%r{^.*$}, '\&*')
 
     var_cmd = "powershell @(get-childitem certificate::localmachine/my ^| where-object { $_.issuer -like '#{issuer_subject}'" \
+      " -and $_.EnhancedKeyUsageList.Contains([Microsoft.PowerShell.Commands.EnhancedKeyUsageRepresentation]::new('Server Authentication','1.3.6.1.5.5.7.3.1')) " \
       " -and $_.subject -eq 'CN=#{Facter['fqdn'].value}' -and $_.hasprivatekey} ^| sort-object -property notafter -descending)[0].thumbprint"
     stdout_str, = exec_call(var_cmd)
     var_stdout_raw = stdout_str
